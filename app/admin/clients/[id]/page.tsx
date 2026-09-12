d8013e4d0f396ae9b8c3e2cdfe16eb4b13aff133
SMcLoud Repo's <stachys@mcloudconstruction.com>
2026-09-12 10:19:15 -0500

    Delete app/admin/clients directory

--- app/admin/clients/[id]/page.tsx
+++
@@ -1,5 +0,0 @@
-import { redirect } from 'next/navigation';
-
-export default function ClientIndexPage({ params }: { params: { id: string } }) {
-  redirect(`/admin/clients/${params.id}/gallery`);
-}
